4418c31a90527d7fe1f9f825475c8c42f01b75fc
ImExtends <sharosari@gmail.com>
2020-10-19 17:32:13 +0200

    testing things

--- default.nix
+++ default.nix
@@ -1,10 +1,14 @@
-{ ... }: {
+{ inputs, ... }: {
 
   imports = [ ./boot ./users ];
 
   #Needed
   nixosUsers.extends = true;
   boot.enable = true;
+
+  environment.systemPackages = [
+    inputs.master.discord
+  ];
 
   system.stateVersion = "21.03";
 }
