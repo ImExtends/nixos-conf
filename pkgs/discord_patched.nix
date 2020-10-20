c851e85ae992225a5086283edd0e128445497601
ImExtends <sharosari@gmail.com>
2020-10-20 11:57:47 +0200

    adding discord patch to open links

---
+++ pkgs/discord_patched.nix
@@ -0,0 +1,4 @@
+with import <nixpkgs> {}:
+pkgs.master.discord.override {
+  nss = pkgs.stable.nss;
+}
--- pkgs/nixpkgs.nix
+++ pkgs/nixpkgs.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    master.discord
+    (import ./discord_patched.nix {})
   ];
 }
