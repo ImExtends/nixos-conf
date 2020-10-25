c7c8ec22fc09cc8ae3186098e1acc162f5bc2a2d
ImExtends <sharosari@gmail.com>
2020-10-25 19:04:44 +0100

    pinning discord to unstable

--- pkgs/nixpkgs.nix
+++ pkgs/nixpkgs.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    (import ./discord_patched.nix { inherit pkgs; })
+    #(import ./discord_patched.nix { inherit pkgs; })
+    discord
   ];
 }
