15179e567785c1e7ad16e88c15c4535c29432364
ImExtends <sharosari@gmail.com>
2021-04-10 18:08:16 +0200

    trying things

--- home-configuration/extends/profiles/rice/xmonad/xmonad.hs
+++ home-configuration/extends/profiles/rice/xmonad/xmonad.hs
@@ -29,6 +29,12 @@ import System.IO
 
 import qualified XMonad.StackSet as W
 
+--Polybar imports
+import qualified Codec.Binary.UTF8.String              as UTF8
+import qualified DBus                                  as D
+import qualified DBus.Client                           as D
+import           XMonad.Hooks.DynamicLog
+
 myTerminal      = "kitty"
 
 myFocusFollowsMouse :: Bool
@@ -141,8 +147,6 @@ myManageHook = composeAll . concat $
 
 myEventHook = mempty
 
-myLogHook = return ()
-
 data LibNotifyUrgencyHook = LibNotifyUrgencyHook deriving (Read, Show)
 
 instance UrgencyHook LibNotifyUrgencyHook where
@@ -164,7 +168,11 @@ myStartupHook = do
 
 myWorkspaces = map show [1..10]
 
-main = xmonad $ azertyConfig
+main :: IO()
+main = mkDbusClient >>= main'
+
+main' :: D.Client -> IO()
+main' dbus = xmonad $ azertyConfig
          {
             terminal           = myTerminal,
             focusFollowsMouse  = myFocusFollowsMouse,
@@ -176,7 +184,45 @@ main = xmonad $ azertyConfig
             layoutHook         = myLayout,
             manageHook         = myManageHook,
             handleEventHook    = myEventHook,
-            logHook            = myLogHook,
+            logHook            = myPolybarLogHook dbus,
             startupHook        = myStartupHook
           } `additionalKeysP` myKeys
 	    `additionalMouseBindings` myMouseBindings
+
+mkDbusClient :: IO D.Client
+mkDbusClient = do
+  dbus <- D.connectSession
+  D.requestName dbus (D.busName_ "org.xmonad.log") opts
+  return dbus
+ where
+  opts = [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+
+-- Emit a DBus signal on log updates
+dbusOutput :: D.Client -> String -> IO ()
+dbusOutput dbus str =
+  let opath  = D.objectPath_ "/org/xmonad/Log"
+      iname  = D.interfaceName_ "org.xmonad.Log"
+      mname  = D.memberName_ "Update"
+      signal = (D.signal opath iname mname)
+      body   = [D.toVariant $ UTF8.decodeString str]
+  in  D.emit dbus $ signal { D.signalBody = body }
+
+polybarHook :: D.Client -> PP
+polybarHook dbus =
+  let wrapper c s | s /= "NSP" = wrap ("%{F" <> c <> "} ") " %{F-}" s
+                  | otherwise  = mempty
+      blue   = "#2E9AFE"
+      gray   = "#7F7F7F"
+      orange = "#ea4300"
+      purple = "#9058c7"
+      red    = "#722222"
+  in  def { ppOutput          = dbusOutput dbus
+          , ppCurrent         = wrapper blue
+          , ppVisible         = wrapper gray
+          , ppUrgent          = wrapper orange
+          , ppHidden          = wrapper gray
+          , ppHiddenNoWindows = wrapper red
+          , ppTitle           = shorten 100 . wrapper purple
+          }
+
+myPolybarLogHook dbus = myLogHook <+> dynamicLogWithPP (polybarHook dbus)
