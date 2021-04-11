4dde298db1cde2741c604ad39ed338f76f9cd70e
ImExtends <sharosari@gmail.com>
2021-04-11 18:47:52 +0200

    adding files

--- home-configuration/extends/profiles/rice/awesome/files.nix
+++ home-configuration/extends/profiles/rice/awesome/files.nix
@@ -4,4 +4,7 @@
   xdg.configFile."awesome/utils/start.lua".text = builtins.readFile ./utils/start.lua;
   xdg.configFile."awesome/bar/init.lua".text = builtins.readFile ./bar/init.lua;
   xdg.configFile."awesome/bar/wibar.lua".text = builtins.readFile ./bar/wibar.lua;
+  xdg.configFile."awesome/bindings/init.lua".text = builtins.readFile ./bindings/init.lua;
+  xdg.configFile."awesome/bindings/keybindings.lua".text = builtins.readFile ./bindings/keybindings.lua;
+  xdg.configFile."awesome/bindings/mousebindings.lua".text = builtins.readFile ./bindings/mousebindings.lua;
 }
