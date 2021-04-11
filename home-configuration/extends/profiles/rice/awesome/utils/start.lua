db619dc9acc553b5ac4deaa1a719713cc984eba8
ImExtends <sharosari@gmail.com>
2021-04-11 13:37:20 +0200

    adding tags

--- home-configuration/extends/profiles/rice/awesome/utils/start.lua
+++ home-configuration/extends/profiles/rice/awesome/utils/start.lua
@@ -1,5 +1,13 @@
 local awful = require("awful")
 
+-- Screen Padding and Tags
+screen.connect_signal("request::desktop_decoration", function(s)
+    -- Screen padding
+    screen[s].padding = {left = 0, right = 0, top = 0, bottom = 0}
+    -- Each screen has its own tag table.
+    awful.tag({"1", "2", "3", "4", "5", "6", "7", "8", "9"}, s, awful.layout.layouts[1])
+end)
+
 do
 	local cmds = 
 	{
