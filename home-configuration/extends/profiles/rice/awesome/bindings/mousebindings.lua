f05bac31eb978cb6044b175979db5440b3316af2
ImExtends <sharosari@gmail.com>
2021-04-11 19:06:56 +0200

    oops

--- home-configuration/extends/profiles/rice/awesome/bindings/mousebindings.lua
+++ home-configuration/extends/profiles/rice/awesome/bindings/mousebindings.lua
@@ -8,4 +8,4 @@ root.buttons(gears.table.join(
     awful.button({ modkey }, 3, awful.mouse.client.resize),
     awful.button({ modkey }, 1, awful.mouse.client.move),
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end)
-)
+))
