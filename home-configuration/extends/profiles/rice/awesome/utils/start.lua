76dce52f930bdd4025f3901f35211834e6ebd71d
ImExtends <sharosari@gmail.com>
2021-04-11 13:57:07 +0200

    oops, wrong tag

--- home-configuration/extends/profiles/rice/awesome/utils/start.lua
+++ home-configuration/extends/profiles/rice/awesome/utils/start.lua
@@ -22,6 +22,6 @@ end
 
 
 awful.rules.rules = {
-	{ rule = { instance = "chromium-browser" }, properties = { screen = 2 } },
-	{ rule = { instance = "discord" }, properties = { screen = 3 } }
+	{ rule = { instance = "chromium-browser" }, properties = { tag = "2" } },
+	{ rule = { instance = "discord" }, properties = { tag = "3" } }
 }
