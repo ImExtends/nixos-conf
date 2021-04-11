9e2231c0ef353e318bf8e181300f0fa621d0ce17
ImExtends <sharosari@gmail.com>
2021-04-11 20:24:08 +0200

    changing method to append client keybindings

--- home-configuration/extends/profiles/rice/awesome/bindings/mousebindings.lua
+++ home-configuration/extends/profiles/rice/awesome/bindings/mousebindings.lua
@@ -4,7 +4,7 @@ local gears = require("gears")
 root.buttons(gears.table.join(
     awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev),
+    awful.button({ }, 5, awful.tag.viewprev)
 ))
 
 client.connect_signal("request::default_mousebindings", function()
