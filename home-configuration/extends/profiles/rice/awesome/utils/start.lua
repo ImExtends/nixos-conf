899967edf33acce9a09665674d8a619a5aa233a5
ImExtends <sharosari@gmail.com>
2021-04-11 13:48:13 +0200

    adding tags

--- home-configuration/extends/profiles/rice/awesome/utils/start.lua
+++ home-configuration/extends/profiles/rice/awesome/utils/start.lua
@@ -6,10 +6,6 @@ screen.connect_signal("request::desktop_decoration", function(s)
     screen[s].padding = {left = 0, right = 0, top = 0, bottom = 0}
 end)
 
-awful.screen.connect_for_each_screen(function(s)
-	awful.tag({"1", "2", "3", "4", "5", "6", "7", "8", "9"}, s, awful.layout.layouts[1])
-end)
-
 do
 	local cmds = 
 	{
