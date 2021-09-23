c693ddaeb0aee7c1635afdb11a2946e6d8231a5c
ImExtends <sharosari@gmail.com>
2021-09-23 10:43:54 +0200

    correcting errors

--- pkgs/nur.nix
+++ pkgs/nur.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [
+    pkgs.nur.repos.extends.bashtop
+    pkgs.nur.repos.extends.cordless
     pkgs.nur.repos.extends.giph
 
     pkgs.nur.repos.kampka.nixify
